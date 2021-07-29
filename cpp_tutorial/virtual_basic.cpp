8877b008b713ad4052fe99fc9a2c2e9a61a79335
HyeonJaeGil <now9728@kaist.ac.kr>
2021-07-30 02:53:56 +0900

    add overriding basic code

--- cpp_tutorial/virtual_basic.cpp
+++ cpp_tutorial/virtual_basic.cpp
@@ -25,25 +25,25 @@ public:
 	void print() { cout << "D" << endl; }
 };
 
-int main() {
-
-	A a;
-	B b;
-	C c;
-	D d;
-
-	// If parent's member function is virtual ,
-	// parent class ptr (ref_a) to child class instance utilize child class's member function.
-	// If parent's member function is not virtual ,
-	// parent class ptr (ref_a) to child class instance utilize parent class's member function.
-	A* ref_a = &b;
-	ref_a->print();
-
-	// Even B - C has no explicit virtual keyword,
-	// as A (hierarchally Top class) has virtual keywords in the member function,
-	// parent class ptr (ref_b) to child class instance utilize child class's member function.
-	C* ref_b = &c;
-	ref_b->print();
-
-	return 0;
-}
+//int main() {
+//
+//	A a;
+//	B b;
+//	C c;
+//	D d;
+//
+//	// If parent's member function is virtual ,
+//	// parent class ptr (ref_a) to child class instance utilize child class's member function.
+//	// If parent's member function is not virtual ,
+//	// parent class ptr (ref_a) to child class instance utilize parent class's member function.
+//	A* ref_a = &b;
+//	ref_a->print();
+//
+//	// Even B - C has no explicit virtual keyword,
+//	// as A (hierarchally Top class) has virtual keywords in the member function,
+//	// parent class ptr (ref_b) to child class instance utilize child class's member function.
+//	C* ref_b = &c;
+//	ref_b->print();
+//
+//	return 0;
+//}
